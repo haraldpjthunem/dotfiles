b0e12a1a9481d2399d4611525c66559f2f1390db
Harald P-J Thunem <harald@thunem.net>
2021-11-26 20:12:16 +0100

    Added onlyoffice install

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -4,6 +4,7 @@ cd programs/
 ./grub-customizer.sh
 ./python.sh
 ./codium.sh
+./onlyoffice.sh
 # ./etcher.sh
 # ./docker.sh
 # ./sublime-text.sh
---
+++ scripts/programs/onlyoffice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# OnlyOffice MS Office replacement
+
+# Add the GPG key of the repository
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+
+# Add the repository
+echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+
+# Update then install vscodium
+sudo apt update && sudo apt install onlyoffice-desktopeditors
