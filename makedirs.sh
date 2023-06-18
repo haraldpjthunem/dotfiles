2a6bb959cdb40486f5b2266fd1bce58ff000047a
Harald P-J Thunem <harald@thunem.net>
2023-06-18 14:56:54 +0200

    Changed makedirs.sh

--- makedirs.sh
+++ makedirs.sh
@@ -1,11 +1,11 @@
-sudo mkdir /mnt/TERA
-sudo mkdir /mnt/SERIES
-sudo mkdir /mnt/MOVIES2TB
-sudo mkdir /mnt/MOVIES4TB
 sudo mkdir /mnt/FILESERVER
+sudo mkdir /mnt/MISC3TB
+sudo mkdir /mnt/MOVIES
+sudo mkdir /mnt/SERIES
+sudo mkdir /mnt/TERA4
 sudo mkdir /mnt/WEBSERVER
 
-sudo mkdir /media/VM
-sudo mkdir /media/ISOs
 sudo mkdir /media/DATA
+sudo mkdir /media/ISOs
 sudo mkdir /media/MYBOOK
+sudo mkdir /media/VM
